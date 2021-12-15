121eb5dd3e51246cd5e11a6cba8e17225b3718aa
Eduardo Corona <eduardocorona@Eduardos-MacBook-Pro.local>
2021-12-15 15:23:24 -0800

    Connect Database

--- server.js
+++ server.js
@@ -4,10 +4,16 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
 var app = express();
+
+//loads our env variables
+require('dotenv').config();
+
+// connects us to database
+require('./config/database');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
